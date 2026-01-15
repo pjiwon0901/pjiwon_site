f23b8e4083494c65bbc374d2c637c56dbf5fee5b
pjiwon0901 <pjiwon0901@gmail.com>
2026-01-16 04:59:47 +0900

    Delete js directory

--- js/auth.js
+++
@@ -1,7 +0,0 @@
-function login() {
-  const username = document.getElementById('username').value;
-  if (!username) return alert("아이디 입력");
-
-  localStorage.setItem("user", username);
-  location.href = "main.html";
-}
